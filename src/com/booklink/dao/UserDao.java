bcab85955d2c5a221a4e09eea9d3b55ee027cab0
unknown <coguddlf1000@naver.com>
2024-05-21 20:53:19 +0900

    checkout

--- src/com/booklink/dao/UserDao.java
+++ src/com/booklink/dao/UserDao.java
@@ -10,10 +10,11 @@ public class UserDao {
     public void registerUser(UserRegistrationDto dto) {
         // dto에 있는 데이터를 꺼내서 DB 에 저장
         
-        return null;
+        return;
     }
     
     public List<User> findAll() {
         // database에 접근해서 모든 user 정보를 리스트로 반환
+        return null;
     }
 }
