c08a9835c275bf784a5d5c95403bd6a0fd0be311
kim-hoyoung <happy3sk@naver.com>
2024-05-21 18:01:52 +0900

    feat : add user

--- src/com/booklink/model/UserRegistrationDto.java
+++ src/com/booklink/model/UserRegistrationDto.java
@@ -1,24 +1,19 @@
 package com.booklink.model;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-public class User {
-    private Long id;
+public class UserRegistrationDto {
     private String name;
     private String password;
     private String loginId;
     private LocalDateTime registrationDate;
     private String image;
 
-    public User(Long id, String name, String password, String loginId, LocalDateTime registrationDate, String image) {
-        this.id = id;
+    public UserRegistrationDto(String name, String password, String loginId, LocalDateTime registrationDate, String image) {
         this.name = name;
         this.password = password;
         this.loginId = loginId;
         this.registrationDate = registrationDate;
         this.image = image;
     }
-
-    // 래퍼 vs 기본형 user table 보고 클래스로 옮기기.
 }
---
+++ src/com/booklink/service/UserService.java
@@ -0,0 +1,13 @@
+package com.booklink.service;
+
+import com.booklink.model.UserRegistrationDto;
+
+public class UserService {
+
+    public void registrationUser(UserRegistrationDto userRegistrationDto) {
+        ////검증
+        ////dao 접근해서 저장
+    }
+
+
+}
